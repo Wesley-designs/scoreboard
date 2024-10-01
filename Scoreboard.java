a2016ef55a76a471ee2144cd0996b21ac198c211
Wesley-designs <jiewesleyd2789@nycstudents.net>
2024-10-01 13:33:14 +0000

    e

--- Scoreboard.java
+++ Scoreboard.java
@@ -2,23 +2,38 @@ public class Scoreboard {
 
     private String teamOne;
     private String teamTwo;
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam;
 
     public Scoreboard(String one, String two) 
     {
         teamOne = one;
         teamTwo = two;
+        activeTeam = teamOne;
     }
     public static void recordPlay(int play) 
     {
-        boolean activeTeam = true;
 
-        if (play = 0)
+        if (play == 0)
         {
-            activeTeam = !activeTeam;
+            if (activeTeam = teamOne)
+            {
+                activeTeam = teamTwo;
+            }
+            else if (activeTeam = teamTwo)
+            {
+                activeTeam = teamOne;
+            }
         }
         else
         {
-            
+            if (activeTeam = teamOne) {
+                teamOneScore += play;
+            }
+            else if (activeTeam = teamTwo) {
+                teamtTwoScore += play;
+            }
         }
     }
 }
